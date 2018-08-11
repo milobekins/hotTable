55bbd585269b43c46df25b18714784e2c1fb2f7e
Artawood Chitamitara <artawood@airbears2-10-142-100-174.airbears2.1918.berkeley.edu>
2018-08-11 13:29:39 -0700

    minor cleanup

--- waitlist.js
+++ waitlist.js
@@ -13,7 +13,7 @@ var PORT = process.env.PORT || 3000;
 // Sets up the Express app to handle data parsing
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
-=======
+
 // Validates if there are any tables available
 
 
